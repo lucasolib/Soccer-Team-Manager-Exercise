13685dea6b883e9254408a5086c3d43addf71b80
Lucas de Oliveira Batista <lucasolibatista@gmail.com>
2022-12-14 03:53:38 -0300

    Segunda requisição usando outra rota

--- src/app.js
+++ src/app.js
@@ -1,7 +1,22 @@
 const express = require('express');
 
+const teams = [
+  {
+    id: 1,
+    name: 'São Paulo Futebol Clube',
+    initials: 'SPF',
+  },
+  {
+    id: 2,
+    name: 'Clube Atlético Mineiro',
+    initials: 'CAM',
+  },
+];
+
 const app = express();
 
 app.get('/', (req, res) => res.status(200).json({ message: 'Olá Mundo!' }));
+
+app.get('/teams', (req, res) => res.status(200).json({ teams }));
 
 module.exports = app;
